9a7c2f95004443cdd837f770998895af705eb5e6
dlwngjs <dlwngjs1227@gmail.com>
2024-06-01 20:11:50 +0900

    네이버 블로그 하나에서 추출한 문제와 답 csv파일로 저장

--- quizgame/src/crawling/DataCrowling.py
+++ quizgame/src/crawling/DataCrowling.py
@@ -15,22 +15,27 @@ txt = combined_text.split("구글", 1)[1]
 txt2 = txt.split('+', 1)[0]
 txt3 = txt2.split('#', 1)[0]
 # print(txt2)
-pattern = re.compile(r"\d+\.\s*([^?]+\?)")
-questions = pattern.findall(txt2)
-
+q_pattern = re.compile(r"\d+\.\s*([^?]+\?)")
+questions = q_pattern.findall(txt2)
+cleaned_questions = []
 for idx, question in enumerate(questions, start=1):
     # 물음표를 기준으로 나누고 두 번째 부분을 가져옴
     cleaned_question = question.split('?', 1)[0] + '?'
+    cleaned_questions.append(cleaned_question.strip())
     print(f"{idx}. {cleaned_question.strip()}")
 
-pattern = r'답\s*:\s*.*?(?=\s*<[^\s]*|\s*\d+\.\s*|\s*$)'
-matches = re.findall(pattern, txt3)
+a_pattern = r'답\s*:\s*.*?(?=\s*<[^\s]*|\s*\d+\.\s*|\s*$)'
+matches = re.findall(a_pattern, txt3)
+
+cleaned_answers = []
+
 for match in matches:
     cleaned_answer = match.replace('답 :', '').strip()
-    print(cleaned_answer)
+    cleaned_answers.append(cleaned_answer)
+    # print(cleaned_answer)
 
-
-# output = "data.csv"
-# csv_open = open(output,'w+', encoding='utf-8')
-# csv_writer = csv.writer(csv_open)
-# csv_writer.writerow(('question','answer'))
+with open('quizgame\src\crawling\data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Question', 'Answer'])
+    for question, answer in zip(cleaned_questions, cleaned_answers):
+        writer.writerow([question, answer])
