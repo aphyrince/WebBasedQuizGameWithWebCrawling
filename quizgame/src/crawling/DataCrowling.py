61de2de534d7526e42a931607a492450737178d3
dlwngjs <dlwngjs1227@gmail.com>
2024-06-01 20:04:17 +0900

    빈 csv파일 추가 / 네이버 블로그 하나에서 답 추출

--- quizgame/src/crawling/DataCrowling.py
+++ quizgame/src/crawling/DataCrowling.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import re, csv
 
 url = "https://m.blog.naver.com/wlstmf2306/222408435436"  # 실제 퀴즈 웹사이트 URL로 대체
 res = requests.get(url)
@@ -13,6 +13,7 @@ combined_text = ' '.join(p.text for p in p)
 txt = combined_text.split("구글", 1)[1]
 # print(txt)
 txt2 = txt.split('+', 1)[0]
+txt3 = txt2.split('#', 1)[0]
 # print(txt2)
 pattern = re.compile(r"\d+\.\s*([^?]+\?)")
 questions = pattern.findall(txt2)
@@ -21,3 +22,15 @@ for idx, question in enumerate(questions, start=1):
     # 물음표를 기준으로 나누고 두 번째 부분을 가져옴
     cleaned_question = question.split('?', 1)[0] + '?'
     print(f"{idx}. {cleaned_question.strip()}")
+
+pattern = r'답\s*:\s*.*?(?=\s*<[^\s]*|\s*\d+\.\s*|\s*$)'
+matches = re.findall(pattern, txt3)
+for match in matches:
+    cleaned_answer = match.replace('답 :', '').strip()
+    print(cleaned_answer)
+
+
+# output = "data.csv"
+# csv_open = open(output,'w+', encoding='utf-8')
+# csv_writer = csv.writer(csv_open)
+# csv_writer.writerow(('question','answer'))
